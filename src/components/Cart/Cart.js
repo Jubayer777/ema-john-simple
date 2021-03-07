a7191d80a4344a80233532e3126c3310ae22ad41
Jubayer777 <jubayeralmahmud24@gmail.com>
2021-03-07 15:18:17 +0600

    something changed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 
+
 const Cart = (props) => {
     const cart=props.cart;
 
     // const total=cart.reduce( (total,prd)=>total+prd.price,0);
     let total=0;
     for(let i=0;i<cart.length;i++){
-        total=total+cart[i].price;
+        const product=cart[i];
+        total=total+product.price *product.quantity;
     }
     let shipping=0;
     
@@ -37,6 +39,11 @@ const Cart = (props) => {
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + Vat: {formateNumber(tax)}</small></p>
             <p>Total Price: {formateNumber(grandTotal)}</p>
+            <br/>
+            {
+                props.children
+            }
+            
         </div>
     );
 };
